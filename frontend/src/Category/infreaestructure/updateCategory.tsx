4add556ae819598f0f60fdba4c17eca6b02b2525
Álvaro <alvarodeberes@gmail.com>
2023-05-07 12:10:53 +0200

    minor changes

--- frontend/src/Category/infreaestructure/updateCategory.tsx
+++ frontend/src/Category/infreaestructure/updateCategory.tsx
@@ -2,7 +2,7 @@ import { baseUrl } from "../../config";
 import Category from "../domain/Category";
 
 export async function updateProduct(category:Category): Promise<Category>{
-  const response = await fetch(`${baseUrl}/api/product`, {
+  const response = await fetch(`${baseUrl}/api/category`, {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(category),    
