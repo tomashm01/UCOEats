dbc7bf89261050f1b757ace00788726dcc8196d5
Álvaro <alvarodeberes@gmail.com>
2023-05-19 00:03:11 +0200

    login && register front changed with tailwing

---
+++ frontend/src/Shared/styles/colors.tsx
@@ -0,0 +1,10 @@
+const colors = {
+    primary: 'blue-500',
+    secondary: 'blue-700',
+    textPrimary: 'black',
+    textSecondary: 'gray-700',
+    bgPrimary: 'white',
+    bgSecondary: 'gray-100',
+  };
+
+  export default colors;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
